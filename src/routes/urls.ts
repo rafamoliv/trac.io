e45fb4679ebebf6459026f7ff4917a36775f855e
rafamoliv <rafaoliv.ti@gmail.com>
2023-03-08 21:01:34 -0300

    feat: create routes

--- src/routes/urls.ts
+++ src/routes/urls.ts
@@ -1,5 +1,12 @@
 export const privateURL = {
-  HOME: '/'
+  HOME: '/',
+  DASHBOARD: '/',
+  ASSETS: '/assets',
+  WORKORDERS: '/workorders',
+  COMPANIES: '/companies',
+  UNITS: '/units',
+  USERS: './users',
+  SETTINGS: '/settings'
 }
 
 export const publicURL = {
