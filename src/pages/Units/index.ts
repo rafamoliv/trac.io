f5f1bdb55b7177ddc3be3336baff74d29a987663
rafamoliv <rafaoliv.ti@gmail.com>
2023-03-11 16:40:53 -0300

    fix: remove non used unit page

--- src/pages/Units/index.ts
+++
@@ -1,2 +0,0 @@
-export { default } from './Settings'
-export { settingsEn, settingsPtBr } from './locales'
--- src/pages/Units/locales/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as settingsPtBr } from './ptBr'
-export { default as settingsEn } from './en'
