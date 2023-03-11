6e740bc678721c982b4b8347e6b55edf601310cd
rafamoliv <rafaoliv.ti@gmail.com>
2023-03-11 18:53:27 -0300

    feat: add util to get initial letters from user name

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,5 +1,27 @@
+/**
+ * Util to formate date to locale string
+ *
+ * @param {Date} DateToFormat
+ * @returns '14/12/2022, 21:00:00'
+ */
+
 export const dateFormat = (DateToFormat: Date) => {
   if (!DateToFormat) return
   const date = new Date(DateToFormat)
   return date.toLocaleString()
+}
+
+/**
+ * Util to return initial letters from name and lastname
+ *
+ * @param {string} name
+ * @returns 'RO'
+ */
+
+export const initialLettersNameLastname = (name: string) => {
+  if (!name) return
+  return name
+    .match(/(\b\S)?/g)
+    .join('')
+    .toUpperCase()
 }
