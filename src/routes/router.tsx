8aad1f7e079b71a477590dce33cad728389364a2
rafamoliv <rafaoliv.ti@gmail.com>
2023-03-11 22:26:40 -0300

    fix: fixing type error and remove things that was just for development

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,12 +1,17 @@
 import i18next from 'i18next'
-import { useEffect, useContext, lazy, Suspense } from 'react'
+import { useEffect, useContext, lazy, Suspense, ReactElement } from 'react'
 import { Navigate, Outlet, useNavigate, useRoutes } from 'react-router-dom'
 
 import { privateURL, publicURL } from './urls'
 import { AppContext } from '@/context/AppContext'
 import { Loading } from '@/components'
 
+interface ProtectedRouteProps {
+  user: string[] | unknown
+}
+
 // pages
+const SignIn = lazy(() => import('@/pages/SignIn'))
 const Dashboard = lazy(() => import('@/pages/Dashboard'))
 const Settings = lazy(() => import('@/pages/Settings'))
 const Assets = lazy(() => import('@/pages/Assets'))
@@ -15,7 +20,7 @@ const Companies = lazy(() => import('@/pages/Companies'))
 const Users = lazy(() => import('@/pages/Users'))
 
 // eslint-disable-next-line react/prop-types
-const ProtectedRoute = ({ user }) => {
+const ProtectedRoute = ({ user }: ProtectedRouteProps): ReactElement => {
   if (user) {
     return <Outlet />
   }
@@ -23,39 +28,18 @@ const ProtectedRoute = ({ user }) => {
   return <Navigate to={publicURL.SIGNIN} />
 }
 
-// eslint-disable-next-line react/prop-types
-const Signin = ({ onClick }) => {
-  return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '100%'
-      }}
-    >
-      <h3>{i18next.t('globalText:sign') as string}</h3>
-      <button onClick={onClick}>
-        {i18next.t('globalText:sign', { context: 'btn' }) as string}
-      </button>
-    </div>
-  )
-}
-
 const Router = () => {
-  const { user, handleSignIn } = useContext(AppContext)
+  const { user } = useContext(AppContext)
   const navigate = useNavigate()
 
   useEffect(() => {
-    //if (!user) navigate(publicURL.SIGNIN)
-    if (user) navigate(privateURL.DASHBOARD)
+    if (!user) navigate(publicURL.SIGNIN)
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [user])
 
   let routes = useRoutes([
     {
-      element: <ProtectedRoute user={'user'} />,
+      element: <ProtectedRoute user={user} />,
       children: [
         {
           path: privateURL.DASHBOARD,
@@ -89,7 +73,7 @@ const Router = () => {
     },
     {
       path: publicURL.SIGNIN,
-      element: <Suspense fallback={<Loading size='bg' />}><Signin onClick={() => handleSignIn('teste@uhas.com')} /></Suspense>
+      element: <Suspense fallback={<Loading size='bg' />}><SignIn /></Suspense>
     },
     {
       path: '*',
