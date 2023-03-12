3a31e47f6dfd69e5f654713b9073ae428c4d3272
rafamoliv <rafaoliv.ti@gmail.com>
2023-03-11 21:48:59 -0300

    config: update vite and vitest config

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import { resolve as pathResolve } from 'path'
 import react from '@vitejs/plugin-react'
 import { defineConfig, loadEnv } from 'vite'
 
-// https://vitejs.dev/config/
-
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
   const isDev = env.REACT_ENVIRONMENT === 'DEVELOPMENT'
--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,2 +1,14 @@
+import 'vitest-canvas-mock'
+import { vi } from 'vitest'
 import '@testing-library/jest-dom'
 import 'jest-styled-components'
+
+/**
+ * Resize observe config for tests with vitest
+ */
+
+global.ResizeObserver = vi.fn().mockImplementation(() => ({
+  observe: vi.fn(),
+  unobserve: vi.fn(),
+  disconnect: vi.fn()
+}))
