a19cee281221a08d30d76249331408b578823262
rafamoliv <rafaoliv.ti@gmail.com>
2023-03-12 20:50:10 -0300

    fix: fixing theme provider type error

--- src/styles/MyThemeProvider.tsx
+++ src/styles/MyThemeProvider.tsx
@@ -1,9 +1,10 @@
+import { ReactNode } from 'react'
 import { ThemeProvider } from 'styled-components'
 
 import { GlobalStyles } from './global'
 import { theme } from './theme'
 
-export const MyThemeProvider = ({ children }) => {
+export const MyThemeProvider = ({ children }: { children: ReactNode }) => {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
