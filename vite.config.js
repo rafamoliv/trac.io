389c4c7d69b1b5cbdd240ec3015df1b8c1fd4794
rafamoliv <rafaoliv.ti@gmail.com>
2023-03-11 21:59:35 -0300

    fix: fixing vite config issue

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import { resolve as pathResolve } from 'path'
 import react from '@vitejs/plugin-react'
 import { defineConfig, loadEnv } from 'vite'
 
+// https://vitejs.dev/config/
+
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
   const isDev = env.REACT_ENVIRONMENT === 'DEVELOPMENT'
