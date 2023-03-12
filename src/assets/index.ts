17026f28d02b2f0c291c1615ac10acc439c1e911
rafamoliv <rafaoliv.ti@gmail.com>
2023-03-12 15:38:49 -0300

    feat: add lottie animations

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,2 +1,15 @@
+/**
+ * Icons
+ */
+
 export { default as IconTracIO } from './trac.io.svg'
 export { default as IconTracIOSmall } from './trac.io.small.svg'
+
+/**
+ * Animations
+ */
+
+export { default as AnimationWelcome } from './animations/welcome-animation.json'
+export { default as AnimationLogin } from './animations/login-animation.json'
+export { default as AnimationDash } from './animations/dash-animation.json'
+export { default as AnimationLoader } from './animations/loader-animation.json'
