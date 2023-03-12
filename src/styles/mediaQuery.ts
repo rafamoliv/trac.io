c9ccb28acdfd8f5a726460e4c0c2555bb35ac2f2
rafamoliv <rafaoliv.ti@gmail.com>
2023-03-12 19:57:15 -0300

    feat: media query util to resposiveness

---
+++ src/styles/mediaQuery.ts
@@ -0,0 +1,29 @@
+import { theme } from './theme'
+/**
+ * Mediaquery values in min-width format
+ *
+ */
+export const minDevice = {
+  /** min-width: 320px */
+  mobileTn: `(min-width: ${theme.breakpoints.tn})`,
+  /** min-width: 576px */
+  mobileXs: `(min-width: ${theme.breakpoints.xs})`,
+  /** min-width: 768px */
+  mobileSm: `(min-width: ${theme.breakpoints.sm})`,
+  /** min-width: 992px */
+  tablet: `(min-width: ${theme.breakpoints.md})`,
+  /** min-width: 1200px */
+  desktopLg: `(min-width: ${theme.breakpoints.lg})`,
+  /** min-width: 1400px */
+  desktopXl: `(min-width: ${theme.breakpoints.xl})`,
+  /** min-width: 2000px */
+  desktopHg: `(min-width: ${theme.breakpoints.hg})`
+}
+
+/**
+ * Mediaquery values in max-width format
+ */
+export const maxDevice = {
+  /** max-width: 1199px */
+  desktopLg: `(max-width: 74.938rem)`
+}
