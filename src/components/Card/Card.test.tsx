7329316982286271b5616f5dce02fff3d026a5b2
rafamoliv <rafaoliv.ti@gmail.com>
2023-03-13 10:54:57 -0300

    feat: initi card component test

---
+++ src/components/Card/Card.test.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import i18next from 'i18next'
+import { expect, it } from 'vitest'
+
+import { render } from '../../utils/tests'
+
+import { Card } from '.'
+
+const props = {
+    children: i18next.t('translation:foo')
+}
+
+describe('<Card />', () => {
+    describe('Component Render', () => {
+        it('render element', () => {
+            const { container } = render(<Card {...props} />)
+            expect(container.firstChild).toMatchSnapshot()
+        })
+    })
+})
